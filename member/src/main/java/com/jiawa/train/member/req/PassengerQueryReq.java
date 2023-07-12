89a1bffabd25fa719cca044d4390cca2e2791109
GaozyDev <670453367@qq.com>
2023-07-12 20:37:03 +0800

    7.8 增加乘车人列表查询接口，只能查看当前全员自己添加的乘客

---
+++ member/src/main/java/com/jiawa/train/member/req/PassengerQueryReq.java
@@ -0,0 +1,14 @@
+package com.jiawa.train.member.req;
+
+public class PassengerQueryReq {
+
+    private Long memberId;
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+}
