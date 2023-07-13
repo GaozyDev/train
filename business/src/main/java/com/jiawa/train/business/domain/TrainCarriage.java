342e14fd723c8f1ea937eeb5b5da3e1728d31cde
GaozyDev <670453367@qq.com>
2023-07-13 18:01:12 +0800

    9.10 修改火车车箱表，column改成col，车箱改成车厢

--- business/src/main/java/com/jiawa/train/business/domain/TrainCarriage.java
+++ business/src/main/java/com/jiawa/train/business/domain/TrainCarriage.java
@@ -1,58 +1,24 @@
-package com.jiawa.train.business.resp;
+package com.jiawa.train.business.domain;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import java.util.Date;
-import com.fasterxml.jackson.annotation.JsonFormat;
 
-public class TrainCarriageQueryResp {
-
-    /**
-     * id
-     */
-    @JsonSerialize(using= ToStringSerializer.class)
+public class TrainCarriage {
     private Long id;
 
-    /**
-     * 车次编号
-     */
     private String trainCode;
 
-    /**
-     * 厢号
-     */
     private Integer index;
 
-    /**
-     * 座位类型|枚举[SeatTypeEnum]
-     */
     private String seatType;
 
-    /**
-     * 座位数
-     */
     private Integer seatCount;
 
-    /**
-     * 排数
-     */
     private Integer rowCount;
 
-    /**
-     * 列数
-     */
     private Integer colCount;
 
-    /**
-     * 新增时间
-     */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createTime;
 
-    /**
-     * 修改时间
-     */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date updateTime;
 
     public Long getId() {
