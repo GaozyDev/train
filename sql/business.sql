29a201b728b5b4b2fd0d3725ca5c9631d576c2a3
GaozyDev <670453367@qq.com>
2023-07-13 17:09:10 +0800

    9.5 配置RDS增加business库，配置持久层

---
+++ sql/business.sql
@@ -0,0 +1,13 @@
+create database train_business default character set utf8mb4 collate utf8mb4_unicode_ci;
+
+drop table if exists `member`;
+create table `member` (
+    `id` bigint not null comment 'id',
+    `mobile` varchar(11) comment '手机号',
+    primary key (`id`),
+    unique key `mobile_unique` (`mobile`)
+) engine = innodb default charset = utf8mb4 comment = '会员';
+
+
+
+
