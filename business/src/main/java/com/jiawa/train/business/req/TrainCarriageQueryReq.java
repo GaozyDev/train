bc030a69c055f7cea0a6595eac254ecd566065cb
GaozyDev <670453367@qq.com>
2023-07-14 10:59:25 +0800

    9.15 为火车座位页面增加车次查询条件

--- business/src/main/java/com/jiawa/train/business/req/TrainCarriageQueryReq.java
+++ business/src/main/java/com/jiawa/train/business/req/TrainCarriageQueryReq.java
@@ -4,6 +4,16 @@ import com.jiawa.train.common.req.PageReq;
 
 public class TrainCarriageQueryReq extends PageReq {
 
+    private String trainCode;
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
+    }
+
     @Override
     public String toString() {
         return "TrainCarriageQueryReq{" +
--- business/src/main/java/com/jiawa/train/business/req/TrainStationQueryReq.java
+++ business/src/main/java/com/jiawa/train/business/req/TrainStationQueryReq.java
@@ -4,6 +4,16 @@ import com.jiawa.train.common.req.PageReq;
 
 public class TrainStationQueryReq extends PageReq {
 
+    private String trainCode;
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
+    }
+
     @Override
     public String toString() {
         return "TrainStationQueryReq{" +
